d6ad85a40efdb7731f243a78ae02265c87bc72cc
Yogesh <yogesh.pdy4@gmail.com>
2023-11-08 10:40:56 +0530

    Update auth-server.js

--- auth-server.js
+++ auth-server.js
@@ -49,6 +49,8 @@ app.get('/callback', passport.authenticate('auth0', {failureRedirect: '/login',}
     const payload = req.user.emails[0];
     const target = req.query.target;
     let mydomain = target.split("//")[1];
+  console.log(mydomain);
+  console.log(payload);
     const token = jwt.sign(payload, '577eeb3923cd6b6e2fc5904b70ce596e976c73ba812b48f01f6f355c709e4a4e');
     res.cookie('ssoToken', token, {domain: mydomain, maxAge: 3600000 });
     res.redirect(target);
